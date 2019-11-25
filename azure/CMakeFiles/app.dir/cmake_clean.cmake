a7e5d9db397f7a0fdab5211197db98b214ab845a
Melika S <melikaihelas@gmail.com>
2019-11-24 19:14:42 -0800

    azure hub set up_ pending send fix

---
+++ azure/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/main.c.o"
+  "CMakeFiles/app.dir/bme280.c.o"
+  "CMakeFiles/app.dir/wiring.c.o"
+  "CMakeFiles/app.dir/telemetry.c.o"
+  "CMakeFiles/app.dir/parson.c.o"
+  "app.pdb"
+  "app"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
