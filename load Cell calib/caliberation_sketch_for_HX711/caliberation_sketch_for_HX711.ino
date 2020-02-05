57ad9c6791b8bf6da49967f496dcb1c0177e22d8
Melika S <melikaihelas@gmail.com>
2020-02-04 20:28:36 -0800

    no errors however value readins are constant ( we don;t want constant)

--- load Cell calib/caliberation_sketch_for_HX711/caliberation_sketch_for_HX711.ino
+++ load Cell calib/caliberation_sketch_for_HX711/caliberation_sketch_for_HX711.ino
@@ -1,25 +1,29 @@
 /* Calibration sketch for HX711 */
  
 #include "HX711.h"  // Library needed to communicate with HX711 https://github.com/bogde/HX711
+// tutorial https://www.brainy-bits.com/load-cell-and-hx711-with-arduino/ 
 //#include "HX711.cpp"  
-#define DOUT  5  // Arduino pin 6 connect to HX711 DOUT
-#define CLK  4  //  Arduino pin 5 connect to HX711 CLK
- 
-HX711 scale();  // Init of library
+#define DOUT  6  // Arduino pin 6 connect to HX711 DOUT
+#define CLK  5  //  Arduino pin 5 connect to HX711 CLK
+
+HX711 scale;  // Init of library
 
 void setup() {
   Serial.begin(9600);
-  //scale.begin(DOUT,CLK);
+  scale.begin(DOUT,CLK);
 void set_scale();  // Start scale
  void tare();       // Reset scale to zero
 
 }
  
 void loop() {
-float  get_units();
+float get_units(20);
 float current_weight;
- current_weight=get_units();  
+ current_weight=get_units;  
   
   float scale_factor=(current_weight/0.145);  // divide the result by a known weight
-  Serial.println(scale_factor);  // Print the scale factor to use
+  Serial.println(scale_factor); 
+  Serial.print(F("scale factor"));// Print the scale factor to use
+  Serial.println(current_weight);
+  Serial.print(F("current weight"));
 }
