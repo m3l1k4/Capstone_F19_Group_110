f9639d7868033803213a011a9844dd4ad281f0b9
Melika S <melikaihelas@gmail.com>
2020-02-04 18:30:09 -0800

    calib sketch debugging
    libraries are out of date

--- load Cell calib/caliberation_sketch_for_HX711/caliberation_sketch_for_HX711.ino
+++ load Cell calib/caliberation_sketch_for_HX711/caliberation_sketch_for_HX711.ino
@@ -2,19 +2,20 @@
  
 #include "HX711.h"  // Library needed to communicate with HX711 https://github.com/bogde/HX711
  
-#define DOUT  2  // Arduino pin 6 connect to HX711 DOUT
-#define CLK  3  //  Arduino pin 5 connect to HX711 CLK
+#define DOUT  5  // Arduino pin 6 connect to HX711 DOUT
+#define CLK  4  //  Arduino pin 5 connect to HX711 CLK
  
-HX711 scale(DOUT, CLK);  // Init of library
-
+//HX711 scale(DOUT, CLK);  // Init of library
+HX711 get_units();
 void setup() {
   Serial.begin(9600);
-  scale.set_scale();  // Start scale
-  scale.tare();       // Reset scale to zero
+  HX711 set_scale();  // Start scale
+HX711  tare();       // Reset scale to zero
+
 }
 
 void loop() {
-  float current_weight=scale.get_units(20);  // get average of 20 scale readings
+  float current_weight=get_units(20);  // get average of 20 scale readings
   float scale_factor=(current_weight/0.145);  // divide the result by a known weight
   Serial.println(scale_factor);  // Print the scale factor to use
 }
