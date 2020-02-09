2e0672d5d25211ea574830060fe79230ffce9b52
ammar369 <55374117+ammar369@users.noreply.github.com>
2020-02-08 19:06:18 -0800

    Update Ardunio_serial_transceiver.ino

--- raspberrypi/Ardunio_serial_transceiver.ino
+++ raspberrypi/Ardunio_serial_transceiver.ino
@@ -1,13 +1,28 @@
-int i = 0;
+String starts, ends, initial, msg, check, sending;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Hello World!");
 
+  starts = String("!!!");
+  ends = String("???");
+  initial = String("incorrect code");
+  msg = String("DATA");
+  check = String("code");
 }
 
+
+
 void loop() {
-  Serial.println("Counter = ");
-  Serial.println(i++);
-  delay(1000);
+  if(Serial.available() > 0) {
+  //Serial.write(Serial.read());
+   String str = Serial.readString();
+   if (str == check) {
+    sending = starts + msg + ends;
+    }
+    else {
+     sending = starts + initial + ends;
+      }
+    Serial.print(sending);
+    //Serial.print(str.length());
+    }
 }
